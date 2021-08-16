257c58097a80629773ae8ba627cce6aeba59558f
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-17 01:41:25 +0600

    added logics for input value

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,14 @@
-console.log("connected");
+//get User Input Value
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const inputAmount = parseFloat(inputAmountText);
+    return inputAmount;
+}
+
+//deposit amount
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositAmount = getInputValue('deposit-input');
+    //console.log(depositAmount);
+    const totalDepositAmount = updateTotal('deposit-total');
+})
