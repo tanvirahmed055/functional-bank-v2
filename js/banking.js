6ce3d002799730a9be3eb936dcdecca28ae17a92
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-17 02:46:56 +0600

    added withdraw logics

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,26 @@
-//get Total Deposit or Withdraw Amount
+//get current balance
+
+function getCurrentBalance() {
+    const balanceElement = document.getElementById('balance-total');
+    const balanceElementText = balanceElement.innerText;
+    const balanceAmount = parseFloat(balanceElementText);
+    return balanceAmount;
+}
+
+
+//update balance total
+
+function updateBalance(depositAmount, isAdd) {
+    const balanceElement = document.getElementById('balance-total');
+    const previousBalance = getCurrentBalance();
+    if (isAdd == true) {
+        balanceElement.innerText = previousBalance + depositAmount;
+    } else if (isAdd == false) {
+        balanceElement.innerText = previousBalance - depositAmount;
+    }
+}
+
+//update Total Deposit or Withdraw Amount
 function updateTotal(totalId, amount) {
     const getTotalField = document.getElementById(totalId);
     const getTotalAmountText = getTotalField.innerText;
@@ -21,4 +43,19 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     //console.log(depositAmount);
     const totalDepositAmount = updateTotal('deposit-total', depositAmount);
 
-})
+    const currentBalance = getCurrentBalance();
+
+    const totalBalance = updateBalance(depositAmount, true);
+});
+
+
+//withdraw amount
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawAmount = getInputValue('withdraw-input');
+    //console.log(withdrawAmount);
+    const totalwithdrawAmount = updateTotal('withdraw-total', withdrawAmount);
+
+    const currentBalance = getCurrentBalance();
+
+    const totalBalance = updateBalance(withdrawAmount, false);
+});
